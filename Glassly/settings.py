90ab375fdfa041c81009fa525cac11c4c0d92edd
Ufuk Kirmizigedik <67425232+ufukkirmizigedik@users.noreply.github.com>
2024-11-20 11:35:14 +0300

    Update settings.py

--- Glassly/settings.py
+++ Glassly/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@mfgir7ykn=(zp42jgi^&0*itr&4ftwfznrb6j0iqpc_k3(-3-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
