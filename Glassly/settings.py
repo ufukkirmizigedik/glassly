bfec01b6b46d892a67f04c36689da9a1426acba8
Ufuk Kirmizigedik <67425232+ufukkirmizigedik@users.noreply.github.com>
2025-01-11 00:32:26 +0300

    Update settings.py

--- Glassly/settings.py
+++ Glassly/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@mfgir7ykn=(zp42jgi^&0*itr&4ftwfznrb6j0iqpc_k3(-3-'
+SECRET_KEY = 'your secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
